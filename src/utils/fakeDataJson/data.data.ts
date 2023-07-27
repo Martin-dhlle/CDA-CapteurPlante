a2f3ebe9cd6f14c4417abe49a676ecd3516b4174
martin-dhlle <mdhollande@fabriquenumerique.fr>
2023-07-27 16:59:37 +0200

    création fakeData

---
+++ src/utils/fakeDataJson/data.data.ts
@@ -0,0 +1,10 @@
+import Data from "../interfaces/Data";
+
+export const data: Data[] = [
+  {
+    _id: "ks4iv9",
+    humidityRate: 90,
+    createdAt: new Date("25/01/2023"),
+    updatedAt: new Date("25/01/2023"),
+  },
+];
---
+++ src/utils/fakeDataJson/sensors.data.ts
@@ -0,0 +1,6 @@
+import Sensor from "../interfaces/Sensor";
+import { data } from "./data.data";
+
+export const sensors: Sensor[] = [
+  { _id: "d445asd", serialNumber: "84D2AD63", Data: data },
+];
