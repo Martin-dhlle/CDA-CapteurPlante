f97d95771e325e0c0aa89e009a7ceecb9622b792
martin-dhlle <mdhollande@fabriquenumerique.fr>
2023-08-16 16:10:21 +0200

    ajout electron

--- src/Components/actual-data/actual-data.component.tsx
+++ src/Components/actual-data/actual-data.component.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import ScalableBox from "../UI/scalable-box/scalable-box.component";
 import Sensor from "../../utils/interfaces/Sensor";
 import { ComponentProperties } from "../../hooks/useComponentSize";
+import { Button } from "@mui/material";
 
 const ActualData: FC<{
   sensor: Sensor;
@@ -10,10 +11,24 @@ const ActualData: FC<{
   if (componentProperties.isClosed) return null;
   return (
     <ScalableBox size={componentProperties.size}>
-      <p>Numéro de série : {sensor.serialNumber}</p>
-      {sensor.name && <p>nom du capteur : {sensor.name}</p>}
-      <p>Taux d'humidité : {sensor.data[0].humidityRate} %</p>
-      <p>{sensor.data[0].updatedAt.toLocaleString()}</p>
+      <div>
+        <p>Numéro de série : {sensor.serialNumber}</p>
+        {sensor.name && <p>nom du capteur : {sensor.name}</p>}
+        <p>Taux d'humidité : {sensor.data[0].humidityRate} %</p>
+        <p>{sensor.data[0].updatedAt.toLocaleString()}</p>
+        <Button type="button" variant="contained">
+          Historique et statistiques
+        </Button>
+      </div>
+      <div>
+        <p>Paramètres du capteur</p>
+        <Button type="button" variant="contained">
+          Changer le type de plante
+        </Button>
+        <Button type="button" variant="contained">
+          Changer l'intervalle
+        </Button>
+      </div>
     </ScalableBox>
   );
 };
